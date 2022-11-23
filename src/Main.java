962456b65671e2bedd925253124965f7a5b59f13
q2q2q8 <art-kash@mail.ru>
2022-11-23 19:06:34 +0300

    третье задание сделано

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,18 @@
-import java.time.LocalDate;
-
 public class Main {
 
-    public static int getClientOS() {
-        int clientOs = 1;
-        return clientOs;
-    }
-
-    public static int getYear() {
-        int currentYear = LocalDate.now().getYear();
-        return currentYear;
+    public static int getDistance() {
+        int deliveryDistance = 95;
+        return deliveryDistance;
     }
 
     public static void main(String[] args) {
-        int os = getClientOS();
-        int year = getYear();
-        if (os == 1 && year < 2022) {
-            System.out.println("Установите облегченную версию приложения для Android");
-        } else if (os == 1 && year >= 2022) {
-            System.out.println("Установите версию приложения для Android");
-        }
-        if (os == 0 && year < 2022) {
-            System.out.println("Установите облегченную версию приложения для iOS");
-        } else if (os == 0 && year >= 2022) {
-            System.out.println("Установите версию приложения для iOS");
-
+        int distance = getDistance() ;
+        if (distance <= 20) {
+            System.out.println("Потребуется дней: 1");
+        } else if (distance > 20 && distance <= 60) {
+            System.out.println("Потребуется дней: 2");
+        } else if (distance > 60 && distance <= 100) {
+            System.out.println("Потребуется дней: 3");
         }
 
         }
