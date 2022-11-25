b27ee7ca22f31e4cad903c7a06ead27c14b3f1c4
q2q2q8 <art-kash@mail.ru>
2022-11-25 18:04:19 +0300

    первое задание сделано

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
 public class Main {
 
     public static int getYear() {
-        int year = 2020;
+        int year = 2021;
         return year;
 
     }
 
+    public static boolean isLeap() {
+       int year = getYear();
+       return  ((year & 3) == 0) && ((year % 100) != 0 || (year % 400) == 0);
+    }
     public static void main(String[] args) {
-
         int year = getYear();
-        if (year % 4 == 0 && year % 100 != 0) {
-            System.out.println(year + "  - високосный год");
-        } else if (year % 100 == 0 && year % 400 == 0) {
+        if (isLeap()) {
             System.out.println(year + " - високосный год");
         } else {
             System.out.println(year + " - не високосный год");
         }
     }
-}
+
+    }
