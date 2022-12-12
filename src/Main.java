826aa90707d584ddf6d4ce288a8cc3e4e036b3aa
q2q2q8 <art-kash@mail.ru>
2022-12-12 11:49:40 +0300

    исправил

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 public class Main {
 
     public static int getDistance() {
-        int deliveryDistance = 150;
+        int deliveryDistance = 45;
         return deliveryDistance;
+    }
+
+    public static int distance() {
+        int distance = 0;
+        int distanceFinal = distance + 40;
+        return distanceFinal;
+    }
+    public static int days() {
+        int days = 0;
+        for (int i = 0; i < 0; i++) {
+        }
+        return days;
     }
 
     public static void main(String[] args) {
@@ -11,8 +23,10 @@ public class Main {
             System.out.println("Потребуется дней: 1");
         } else if (distance > 20 && distance <= 60) {
             System.out.println("Потребуется дней: 2");
-        } else if (distance > 60) {
+        } else if (distance > 60 && distance <= 100) {
             System.out.println("Потребуется дней: 3");
+        } else if (distance > 100) {
+            System.out.println("К сожалению, так далеко мы не доставляем.");
         }
 
         }
